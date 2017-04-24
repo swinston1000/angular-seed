a059ea7b8744b2b1f9971ae48f42aa5a326d4f43
Steven Winston <swinston100@hotmail.com>
2017-04-24 14:55:18 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ gulp.task('default', function() {
   var port = process.env.PORT || 8000;
   gulp.src('')
     .pipe(shell([
-      './node_modules/http-server/bin/http-server ./app -c-1 -p ' + port
+      'http-server ./app -c-1 -p ' + port
     ]))
 })
